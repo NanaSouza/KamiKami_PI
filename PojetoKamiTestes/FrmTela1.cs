0c21c17386e661b7197870f7dd07fc965894cd31
VIVIAN DOS SANTOS SOUZA <vivian.ssouza7@senacsp.edu.br>
2026-05-29 19:40:42 -0300

    Projeto atualizado

--- PojetoKamiTestes/FrmTela1.cs
+++ PojetoKamiTestes/FrmTela1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace PojetoKamiTestes
 {
     public partial class Frm_principal : Form
     {
+        string kamikami = "server=localhost;uid=root;password=;database=kamikami;";
         public Frm_principal()
         {
             InitializeComponent();
@@ -24,35 +26,41 @@ namespace PojetoKamiTestes
 
         private void btn_login_Click(object sender, EventArgs e)
         {
+            string cpf = msk_usuario.Text
+            .Replace(".", "")
+            .Replace("-", "")
+            .Trim();
+            string senha = txt_senha.Text;
+
+            if (string.IsNullOrWhiteSpace(cpf) ||
+                string.IsNullOrWhiteSpace(senha))
             {
-                string cpf = msk_usuario.Text
-                .Replace(".", "")
-                .Replace("-", "")
-                .Trim();
-                string senha = txt_senha.Text;
+                MessageBox.Show("Preencha CPF e Senha!");
+                return;
+            }
+            MySqlConnection conn = new MySqlConnection(kamikami);
+            string sql = "SELECT cpf FROM usuario WHERE cpf = @cpf AND senha = @senha";
 
-                if (string.IsNullOrWhiteSpace(cpf) ||
-                   string.IsNullOrWhiteSpace(senha))
-                {
-                    MessageBox.Show("Preencha CPF e Senha!");
-                    return;
-                }
-
-                var usuario = GerenFunc.funcionarios.FirstOrDefault(f =>
-                    f.CPF == cpf &&
-                    f.Senha == senha);
-
-                if (usuario != null)
-                {
-                    CriarPedido tela = new CriarPedido();
-                    tela.Show();
-
-                    this.Hide();
-                }
-                else
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@cpf", cpf);
+                cmd.Parameters.AddWithValue("@senha", senha);
+                var reader = cmd.ExecuteReader();
+                if (reader.GetValue(0) == null)
                 {
                     MessageBox.Show("CPF ou senha inválidos.");
-                }
+                    return;
+                } 
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
             }
         }
 
